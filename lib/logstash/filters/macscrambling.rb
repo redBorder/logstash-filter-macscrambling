ecdf04ce94e65a4ea6da6477bb4292130af6838d
Aaron Pérez <aperez@redborder.com>
2020-03-24 13:58:19 +0000

    Adding structure and files

---
+++ lib/logstash/filters/macscrambling.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+require "logstash/filters/base"
+require "logstash/namespace"
+require "json"
+
+class LogStash::Filters::MacScrambling < LogStash::Filters::Base
+
+  config_name "macscrambling"
+
+
+
+end    # class Logstash::Filter::MacScrambling
