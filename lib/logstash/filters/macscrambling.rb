cd6980dd3fda7e1a951aeb832565644b678f4336
Aarón Pérez <aperez@redborder.com>
2020-03-25 16:31:23 +0000

    Encoding/Decoding hexadecimal values

--- lib/logstash/filters/macscrambling.rb
+++ lib/logstash/filters/macscrambling.rb
@@ -31,22 +31,40 @@ class LogStash::Filters::MacScrambling < LogStash::Filters::Base
     ts_end = Time.now - @ts_start
     if ts_end >= 60 then
       update_salt
+      @ts_start = Time.now
     end
     
     mac = event.get("client_mac")
     spUUID = event.get("service_provider_uuid")
 
-    scramble = @scrambles[spUUID.to_s]["mac_hashing_salt"]
+    begin
+      scramble = @scrambles[spUUID.to_s]["mac_hashing_salt"]
+      
+      if scramble and mac then     
+        # Decode Hexadecimal value, scramble and write to the mac format
+        decoded_mac = [mac.gsub(":","").to_s].pack('H*')
+        decoded_mac_screamble = scramble_mac(decoded_mac)
+        decoded_mac_screamble_to_mac = to_mac(decoded_mac_screamble, ":")
 
-    if scramble and mac then
-
-      #TODO	
-
+        message.put("client_mac",decoded_mac_screamble_to_mac)
+      end
+    rescue
+      @scrambles = Hash.new
+      puts "GeneralSecurityException"
     end
-    
     
   end  # def filter
 
+  def scramble_mac
+    #TODO - PKCS5S2ParametersGenerator
+  end
+
+  def to_mac(value, separator)
+    #TODO - StringBuilder
+
+  end
+
+  # Connect to Postgresql DB to get information from sensors
   def update_salt
     begin
         
@@ -62,11 +80,15 @@ class LogStash::Filters::MacScrambling < LogStash::Filters::Base
           
           #"Property" field has a json embessed
           json_content = JSON.parse(row["property"])
-          salt = json_content["mac_hashing_salt"])
+
+          # Decoding from Hexadecimal to string
+          salt = [json_content["mac_hashing_salt"].to_s].pack('H*')
           
           @scrambles[row["uuid"].to_s] = Hash.new
-          @scrambles[row["uuid"].to_s]["mac_hashing_salt"] = salt.decode('hex') # from Hexadecimal to string
-          @scrambles[row["uuid"].to_s]["mac_prefix"] = @mac_prefix.bytes.to_a   #to Byte array
+          @scrambles[row["uuid"].to_s]["mac_hashing_salt"] = salt  
+
+          if mac_prefix and mac_prefix.to_s.strip.empty?
+            @scrambles[row["uuid"].to_s]["mac_prefix"] = @mac_prefix.bytes.to_a   #to Byte array
         end
       end
 
