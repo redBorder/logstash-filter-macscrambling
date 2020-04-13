e6da05fd38e7c1efc1c8e6ccafdba6380089f0c3
Aarón Pérez <aperez@redborder.com>
2020-04-13 14:34:38 +0100

    removed an used variable

--- lib/logstash/filters/macscrambling.rb
+++ lib/logstash/filters/macscrambling.rb
@@ -28,7 +28,6 @@ class LogStash::Filters::Macscrambling < LogStash::Filters::Base
     @memcached_server = MemcachedConfig::servers.first if @memcached_server.empty?
     @memcached = Dalli::Client.new(@memcached_server, {:expires_in => 0, :value_max_bytes => 4000000}) 
     @scrambles = @memcached.get("scrambles") || {}
-    @mac_prefix = @memcached.get("mac_prefix") || ''
     @last_refresh_stores = nil
     
   end #def register
